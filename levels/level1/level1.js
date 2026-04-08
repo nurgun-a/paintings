5fe1b31fdfec1e47d65fc44545c2c05bfb2c9bea
Ньургун <nurgun.alekseev@bk.ru>
2026-04-08 15:29:14 +0300

    l1_pptx

--- levels/level1/level1.js
+++ levels/level1/level1.js
@@ -763,8 +763,8 @@ addHouseToLayer: function(id, position, imageUrl, onClick) {
             this.soundPortal.play().catch(e => console.log('Звук портала:', e));
         }
         this.playVideo('/levels/level1/assets/videos/portal_open.mp4', false, () => {
-            //window.location.href = '/igs.pptx';
-            window.open('/igs.pptx', '_blank');
+            window.location.href = '/igs.pptx';
+            //window.open('/igs.pptx', '_blank');
         });
     });
     
